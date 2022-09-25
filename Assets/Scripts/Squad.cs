df581acfcb84eb162b5a2a314e877af7cf94c297
CookOS1803 <usovich5321@gmail.com>
2022-09-25 21:12:23 +0300

    sprites fix

--- Assets/Scripts/Squad.cs
+++ Assets/Scripts/Squad.cs
@@ -7,21 +7,28 @@ public class Squad : MonoBehaviour
     [SerializeField] private GameObject soldierPrefab;
     [SerializeField] private Side _side;
     [Zenject.Inject] private Zenject.DiContainer container;
+    private Soldier squadLeader;
     private Soldier[] soldiers;
 
     private void Awake()
     {
-        soldiers = new Soldier[3];
+        soldiers = new Soldier[2];
     }
 
     private void Start()
     {
-        for (int i = 0; i < 3; i++)
+        squadLeader = container.InstantiatePrefab(soldierPrefab, transform.position, transform.rotation, transform).GetComponent<Soldier>();
+        squadLeader.transform.localScale = Vector3.one;
+        squadLeader.side = _side;
+        squadLeader.ChangeItems();
+
+        for (int i = 0; i < soldiers.Length; i++)
         {
             var pos = transform.position;
-            pos.x -= i;
+            pos.x -= (i + 1) * transform.localScale.x;
 
-            soldiers[i] = container.InstantiatePrefab(soldierPrefab, pos, Quaternion.identity, transform).GetComponent<Soldier>();
+            soldiers[i] = container.InstantiatePrefab(soldierPrefab, pos, transform.rotation, null).GetComponent<Soldier>();
+            soldiers[i].transform.localScale = transform.localScale;
             soldiers[i].side = _side;
             soldiers[i].ChangeItems();
 
